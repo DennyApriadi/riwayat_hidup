ef1fab3a4596a589a93c5175d13ba69cbcc09053
DennyApriadi <deniapriadi9a@gmail.com>
2022-06-28 20:54:25 +0700

    first commit

---
+++ link.js
@@ -0,0 +1,19 @@
+function linkWA() {
+    document.getElementById("wa").setAttribute("href", "https://wa.me/6283869547181",);
+}
+linkWA();
+
+function linkIG() {
+    document.getElementById("ig").setAttribute("href","https://instagram.com/denny.com_18",);
+}
+linkIG();
+
+function linkfb() {
+    document.getElementById("fb").setAttribute("href","https://web.facebook.com/profile.php?id=100009508856120",);
+}
+linkfb();
+
+function linkemail() {
+    document.getElementById("email").setAttribute("href", "mailto:deniapriadi9a@gmail.com",);
+}
+linkemail();
